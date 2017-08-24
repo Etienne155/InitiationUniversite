d2c1a0ab61a5d6759ca6574b2cae10fec2439a34
Étienne Boucher <etienneboucher@hotmail.ca>
2017-08-24 16:58:43 -0400

    structure d'animation en place et fonctionnelle

--- app/src/main/java/com/etien/initiationuniversite/MainActivity.java
+++ app/src/main/java/com/etien/initiationuniversite/MainActivity.java
@@ -1,5 +1,9 @@
 package com.etien.initiationuniversite;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.Color;
@@ -7,8 +11,10 @@ import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -24,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
     LinearLayout lLayout;
     ConstraintLayout cLayout;
     ConstraintSet cSet;
+
+    double screenRatio;
+    Float xBias;
+    Float yBias;
+
+    AnimatorSet movingButton;
+    ValueAnimator buttonXAnimation;
+    ValueAnimator buttonYAnimation;
+
     static int count;
 
     @Override
@@ -45,6 +60,13 @@ public class MainActivity extends AppCompatActivity {
         cSet = new ConstraintSet();
         cSet.clone(cLayout);
 
+        xBias = 0.5f;
+        yBias = 0.5f;
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        screenRatio = (double) metrics.widthPixels/metrics.heightPixels;
+
         // Init text
         textView.setText("" + count);
 
@@ -55,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
 
                 // Update count on click
                 textView.setText("" + ++count);
+                bounceAnimation();
 
                 if (count >= 40 && getRandomInt(0,1) == 0) {
                     // change background color
@@ -91,11 +114,12 @@ public class MainActivity extends AppCompatActivity {
                 editor.apply();
             }
         });
+
+        bounceAnimation();
     }
 
 
     public int getRandomInt(int min, int max) {
-
         Random rand = new Random();
         return rand.nextInt((max - min) + 1) + min;
     }
@@ -103,5 +127,85 @@ public class MainActivity extends AppCompatActivity {
     public float getRandomFloat() {
         Random rand = new Random();
         return rand.nextFloat();
+    }
+
+    public void startAnimations(float targetXBias, float targetYBias) {
+        // stopping previous animation
+        if (movingButton != null) {
+            movingButton.removeAllListeners();
+        }
+
+        // creating animations
+        buttonXAnimation = ValueAnimator.ofFloat(xBias, targetXBias);
+        buttonYAnimation = ValueAnimator.ofFloat(yBias, targetYBias);
+
+//        long duration = (long) Math.sqrt(Math.pow((targetXBias - xBias) * screenRatio, 2) + Math.pow(targetYBias - yBias, 2) * 100000.0);
+        long duration = 1000;
+
+        // listener to update button position
+        buttonXAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                xBias = (float) animation.getAnimatedValue();
+                cSet.setHorizontalBias(R.id.buttonCount, xBias);
+                cSet.applyTo(cLayout);
+            }
+        });
+
+        buttonYAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                yBias = (float) animation.getAnimatedValue();
+                cSet.setVerticalBias(R.id.buttonCount, yBias);
+                cSet.applyTo(cLayout);
+            }
+        });
+
+        // Animation coordination
+        movingButton = new AnimatorSet();
+        movingButton.play(buttonXAnimation).with(buttonYAnimation);
+        movingButton.setDuration(duration);
+        movingButton.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                bounceAnimation();
+            }
+        });
+
+        // Starting animation
+        movingButton.start();
+    }
+
+    public void bounceAnimation() {
+        int reboundType;
+
+        if (xBias == 0 || xBias == 1) {
+            reboundType = getRandomInt(0, 2);
+        } else if (yBias == 0 || yBias == 1) {
+            reboundType = getRandomInt(0, 2) + 3;
+        } else {
+            reboundType = getRandomInt(0, 3) + 1;
+        }
+
+        switch (reboundType) {
+            case 0:
+                startAnimations((xBias + 1) % 2, getRandomFloat());
+                break;
+            case 1:
+                startAnimations(getRandomFloat(), 0.0f);
+                break;
+            case 2:
+                startAnimations(getRandomFloat(), 1.0f);
+                break;
+            case 3:
+                startAnimations(0.0f, getRandomFloat());
+                break;
+            case 4:
+                startAnimations(1.0f, getRandomFloat());
+                break;
+            case 5:
+                startAnimations(getRandomFloat(), (yBias + 1) % 2);
+                break;
+        }
     }
 }
